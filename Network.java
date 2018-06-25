78a80a25b770f6a678c423701b04b901f98384f5
AndyT991 <39470633+AndyT991@users.noreply.github.com>
2018-06-25 16:03:58 -0400

    Update Network.java

--- Network.java
+++ Network.java
@@ -6,59 +6,60 @@ import java.util.regex.Pattern;
 public abstract class Network {
 	
 	protected String[] split;
+	//Main array that holds converted addresses 
 	protected ArrayList <Integer> conversion;
-
-	
-	//protected ArrayList <Integer> binary;
-	
 	protected int bits;
 	protected int length;
+	protected int IPClass;
 	
 	public abstract void verify(String x);
 		
-	
+	//This method uses multiple arrays to 
+	//1st: Convert a string into individual elements
+	//2nd Parse those individual string elements into ints 
+	//3rd 
 	public void convertToBits(String x) {
 		ArrayList<Integer> binary = new ArrayList<Integer>();
 		conversion = new ArrayList<Integer>();
+		
 		
 		split = x.split(Pattern.quote("."));
 		
 		length = split.length;
 		
+		//for loop used to add individual numbers into binary array 
 		for( int i=0; i<split.length;i++ ) {
-			binary.add( Integer.parseInt(split[i]));
+			binary.add( Integer.parseInt(split[i]) );
 		}
+		//Assigns first octet to variable IPClass
+		IPClass = binary.get(0);
 		
-		//find a way to accept zeros 
 		for(int i=0; i < binary.size();i++) {
-			
-			if(binary.get(i)==0) {
-				for(int j=0;j<8;j++) {
-					conversion.add(0);
-					continue;
-				}
-			}
+			//array used to temporailly hold values
+			int [] holder = new int[8];
+			int counter = 7;
 			
 			while(binary.get(i)>0) {
-				/*
-				if(binary.get(i)==0) {
-					for(int j=0;j<8;j++) {
-						conversion.add(0);
-						continue;
-					}
-				}
-				*/
+				
 				int num = binary.get(i);
 				int quotient = binary.get(i)/2;
 				num = num % 2;
 				
-				conversion.add(num);
 				binary.set(i,quotient);
+				holder[counter] = num;
+				counter = counter -1;
+			}
+			for(int j=0;j<holder.length;j++) {
+				conversion.add(holder[j]);
 			}
 		}
+		
 		System.out.println(conversion);
+		System.out.println(conversion.size());
 		
 	}
+	
+	
 	public void getBits() {
 		int counter = 0;
 		for(int i=0;i<conversion.size();i++) {
@@ -66,8 +67,9 @@ public abstract class Network {
 				counter++;
 			}
 		}
-		System.out.println(counter);
+		
+		System.out.println("Subnet bits "+counter);
+		counter = bits;
 	}
 	
-
 }
