403263d575e3cb7ed7c9e86871f76b88cf676810
AndyT991 <39470633+AndyT991@users.noreply.github.com>
2018-08-04 17:04:16 -0400

    Update Address.java

--- Address.java
+++ Address.java
@@ -1,8 +1,36 @@
 package subnetting;
 
+import java.util.ArrayList;
+
 public class Address extends Network {
-	int bits;
+	
 	String [] split;
+	private int defaultSubnetMask;
+	private ArrayList <Integer> id;
+	String[] idInOctet;
+	
+	public void classCheck() {
+		
+		System.out.println(IPClass);
+		
+		if(IPClass<126) {
+			System.out.println("Class A");
+			defaultSubnetMask = 8;
+		}
+		else if(IPClass>128 && IPClass<191) {
+			System.out.println("Address is Class B");
+			defaultSubnetMask = 16;
+		}
+		else if(IPClass>192 && IPClass<223) {
+			System.out.println("Address is Class C");
+			defaultSubnetMask = 24;
+		}
+		else if(IPClass>=224 && IPClass<=239) {
+			System.out.println("Address is Class D, Reserved for multicasting");
+		}
+		
+		
+	}
 	
 	public void verify(String address) {
 		if(!address.contains(".")) {
@@ -11,16 +39,38 @@ public class Address extends Network {
 			menu.display();
 		}
 		else {
+			System.out.println("Address in binary");
 			convertToBits(address);
-		}
 		
+		}
+
 	}
 	@Override
 	public void convertToBits(String address) {
 		super.convertToBits(address);
 		
+	}
+	
+	public void findNetworkID(int bits) {
+		id = new ArrayList<Integer>(conversion);
 		
+		//System.out.println(bits);
+		
+		for(int i=0; i < 32-bits; bits++) {
+			id.set(bits, 0);
+		}
+		
+		System.out.println(id);
+	}
+	
+	public int getDefaultSubnetMask() {
+		return defaultSubnetMask;
+	}
+	
+	public ArrayList<Integer> getId(){
+		return id;
 	}
 	
 
 }
+
